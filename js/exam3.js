a6e6a304042df128a1027c4ee5435fa7454b379b
Junhyung Ki <kijoonh91@gmail.com>
2021-10-10 21:57:45 +0200

    ng switch example added

--- js/exam3.js
+++ js/exam3.js
@@ -1,6 +1,7 @@
 var app3 = angular.module('app3', []);
- 
+
 app3.controller('gListCtrl', function($scope) {
+    
     $scope.groceries = [
         {item: "Tomatoes", purchased: false},
         {item: "Potatoes", purchased: false},
