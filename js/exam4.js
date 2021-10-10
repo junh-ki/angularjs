e72072770d788a5961924a02de69e74b9ff387f5
Junhyung Ki <kijoonh91@gmail.com>
2021-10-10 23:01:35 +0200

    table, button, checkbox techniques

--- js/exam4.js
+++ js/exam4.js
@@ -2,6 +2,21 @@ var app4 = angular.module('app4', []);
 
 app4.controller('eventCtrl', function($scope) {
 
+    $scope.dayTimeButton = true;
+
+    $scope.capitals = [
+        {"City": "Montgomery",
+        "State": "Alabama"},
+        {"City": "Juneau",
+        "State": "Alaska"},
+        {"City": "Phoenix",
+        "State": "Arizona"},
+        {"City": "Little Rock",
+        "State": "Arkansas"}
+    ];
+
+    $scope.disableButton = true;
+
     $scope.blur = 0;
     $scope.click = 0;
     $scope.dblclick = 0;
